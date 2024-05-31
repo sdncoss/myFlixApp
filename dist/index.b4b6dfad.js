6bc38244f19bc4d5d360d078a91e4b2305d1109b
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-05-31 11:53:13 -0700

    changed capitol letters again

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27701,10 +27701,10 @@ const LoginView = ({ onLoggedIn })=>{
             body: JSON.stringify(data)
         }).then((response)=>response.json()).then((data)=>{
             console.log("Login response: ", data);
-            if (data.Username) {
+            if (data.user) {
                 localStorage.setItem("user", JSON.stringify(data.user));
                 localStorage.setItem("token", data.token);
-                onLoggedIn(data.Username, data.token);
+                onLoggedIn(data.user, data.token);
             } else alert("No such user");
         }).catch((e)=>{
             console.error("Login error: ", e);
--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -26,10 +26,10 @@ export const LoginView = ({ onLoggedIn }) => {
     }).then((response) => response.json())
       .then((data) => {
         console.log("Login response: ", data);
-        if (data.Username) {
+        if (data.user) {
           localStorage.setItem("user", JSON.stringify(data.user));
           localStorage.setItem("token", data.token);
-          onLoggedIn(data.Username, data.token);
+          onLoggedIn(data.user, data.token);
         } else {
           alert("No such user");
         }
