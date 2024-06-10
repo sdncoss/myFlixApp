8631a546141d805ff43bc00cff7579bc49b84924
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-06-10 14:55:05 -0700

    reverted back

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,36 +1,146 @@
-import react from "react";
-import { UserInfo } from './user-info';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { Button, Card, Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { FavoriteMovies } from './user-favorites';
 import { UpdateUser } from "./update-user";
-import { UserDelete } from './user-delete';
+import { UserDelete } from './user-delete'
 
 
-export const ProfileView = ({ movies, user }) => {
-  if (!user) {
-    return <div>User data not available</div>;
+export const ProfileView = ({ localUser, movies, token }) => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+
+  const [username, setUsername] = useState(storedUser.username);
+  const [email, setEmail] = useState(storedUser.email);
+  const [password, setPassword] = useState(storedUser.password);
+  const [birthday, setBirthdate] = useState(storedUser.birthDate);
+  const [user, setUser] = useState();
+
+  if (!storedUser) {
+    return <div>Loading...</div>;
   }
-  const { Username, Email, Birthday, Password } = user;
+
+
+
+
+  const formData = {
+    Username: username,
+    Password: password,
+    Email: email,
+    Birthday: birthday
+  };
+
+
+  //triggers submit button
+  const handleSubmit = (event) => {
+    event.preventDefault(event);
+    fetch(`https://my-flix-db-975de3fb6719.herokuapp.com/users/${storedUser.Username}`, {
+      method: "PUT",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    }
+    )
+      .then((response) => {
+        if (response.ok) {
+          alert("Update successful");
+          return response.json();
+        }
+        alert("Update failed");
+        throw new Error("Update failed");
+      })
+      .then((updatedUser) => {
+        localStorage.setItem('user', JSON.stringify(updatedUser));
+        setUser(updatedUser);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  //updates user input
+  const handleUpdate = (e) => {
+    switch (e.target.type) {
+      case "text":
+        setUsername(e.target.value);
+        break;
+      case "email":
+        setEmail(e.target.value);
+        break;
+      case "password":
+        setPassword(e.target.value);
+        break;
+      case "date":
+        setBirthdate(e.target.value);
+      default:
+    }
+  }
+
+  //updates profile information 
+  useEffect(() => {
+    if (!token || !storedUser) {
+      return;
+    }
+
+    fetch(`https://my-flix-db-975de3fb6719.herokuapp.com/users/${storedUser.Username}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+        setUsername(data.Username);
+        setEmail(data.Email);
+        setBirthdate(data.Birthday);
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      });
+  }, [storedUser, token]);
+
+
 
   return (
-    <div className="profile-view-container">
-      <div className="user-info">
-        <UserInfo
-          name={user.Username}
-          email={user.Email}
-          birthday={user.Birthday}
-          password={user.Password}
-        />
-      </div>
-      <div className="user-update">
-        <UpdateUser />
-      </div>
-      <div className="user-delete">
-        <UserDelete />
-      </div>
-      <div className="favorite-movies">
-        <FavoriteMovies movies={movies} user={user} />
-      </div>
-    </div>
-  );
+    <Container>
+      <Row>
+        <Card className="mb-5">
+          <Card.Body>
+            <Card.Title className="text-center">My Profile</Card.Title>
+            <Card.Text>
+              <div>
+                <p>Username: {username} </p>
+                <p>Email: {email} </p>
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="mb-5">
+          <Card.Body>
+            <UpdateUser
+              formData={formData}
+              handleUpdate={handleUpdate}
+              handleSubmit={handleSubmit}
+            />
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>
+            <UserDelete />
+          </Card.Body>
+        </Card>
+      </Row>
+      <Row>
+        <Col className="mb-5" xs={12} md={8}>
+          <FavoriteMovies user={user} movies={movies} />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+ProfileView.propTypes = {
+  localUser: PropTypes.object.isRequired,
+  movies: PropTypes.array.isRequired,
+  token: PropTypes.string.isRequired
 };
--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,119 +1,64 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React from 'react'
+import PropTypes from "prop-types"
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
 
 
-
-export const UpdateUser = () => {
-  const [storedUser, setStoredUser] = useState(
-    JSON.parse(localStorage.getItem("user"))
-  );
-  const [storedToken, setStoredToken] = useState(localStorage.getItem("token"));
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [birthday, setBirthday] = useState("");
-
-  useEffect(() => {
-    setStoredUser(JSON.parse(localStorage.getItem("user")));
-    setStoredToken(localStorage.getItem("token"));
-  }, []);
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const token = localStorage.getItem("token");
-    if (!token) {
-      console.log("Token not found in localStorage");
-      return;
-    }
-    const data = {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday,
-    };
-
-    try {
-      const response = await fetch(
-        `https://my-flix-db-975de3fb6719.herokuapp.com/users/${storedUser.Username}`,
-        {
-          method: "PUT",
-          body: JSON.stringify(data),
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      if (response.ok) {
-        alert("User Info Updated!");
-        const updatedUser = await response.json();
-        localStorage.setItem("user", JSON.stringify(updatedUser));
-        setUsername(updatedUser.Username);
-        setPassword(updatedUser.Password);
-        setEmail(updatedUser.Email);
-        setBirthday(updatedUser.Birthday);
-      } else {
-        alert("Update failed!");
-      }
-    } catch (error) {
-      console.error("Error updating user", error);
-      alert("Update failed");
-    }
-  };
+export const UpdateUser = ({ formData, handleUpdate, handleSubmit, handleDeleteAccount }) => {
   return (
-    <Form onSubmit={handleSubmit} >
-      <div>
-        <h1>Update User Information</h1>
-      </div>
-      <Form.Group controlId="formUsername" className="userNameGroup">
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          className="inputUser"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          minLength="3"
-        />
-      </Form.Group>
+    <Row>
+      <Form onSubmit={handleSubmit}>
+        <h3 style={{ textAlign: "center" }}>Update Profile Information</h3>
+        <Form.Group className='mb-2'>
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            minLength={4}
+            value={formData.username}
+            onChange={(e) => handleUpdate(e)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className='mb-2'>
+          <Form.Label>Password:
+            <p>Your new password must be at least 8 characters long.</p>
+          </Form.Label>
+          <Form.Control
+            type="password"
+            minLength={8}
+            value={formData.password}
+            onChange={(e) => handleUpdate(e)}
+            required
+          />
+        </Form.Group >
+        <Form.Group className='mb-2'>
+          <Form.Label> Email: </Form.Label>
+          <Form.Control
+            type="email"
+            value={formData.email}
+            onChange={(e) => handleUpdate(e)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className='mb-2'>
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control
+            type="date"
+            value={formData.birthday}
+            onChange={(e) => handleUpdate(e)}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" >Submit Changes</Button>
+      </Form>
+    </Row>
+  )
+}
 
-      <Form.Group controlId="formPassword" className="userNameGroup">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          className="inputUser"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </Form.Group>
-
-      <Form.Group controlId="formEmail" className="userNameGroup">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          className="inputUser"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </Form.Group>
-
-      <Form.Group controlId="formBirthday" className="userNameGorup">
-        <Form.Label>Birthday</Form.Label>
-        <Form.Control
-          className="inputUser"
-          type="date"
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </Form.Group>
-      <Button variant="primary" onSubmit={handleSubmit}>
-        Submit
-      </Button>
-    </Form>
-  );
+UpdateUser.propTypes = {
+  formData: PropTypes.object.isRequired,
+  handleUpdate: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleDeleteAccount: PropTypes.func.isRequired
 };
