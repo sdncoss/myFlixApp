881e10c862e04d06f27a3f86b62eabe80f5c874a
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-06-10 14:38:25 -0700

    forgot slash info

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 
 
